78a412f5ecbd557c9bf8ed87e7492bb02578356b
Alex Post <alexpost1995@gmail.com>
2019-11-07 15:13:05 +0100

    Re-added popup, added buttons to it

--- been_there_frontend/basic/src/Header.js
+++ been_there_frontend/basic/src/Header.js
@@ -13,17 +13,17 @@ export class Header extends Component {
                 <div className="collapse navbar-collapse" id="navbarText">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="https://alhooimeijer.wixsite.com/beenthere" target="_blank">About been there</a>
+                            <a className="nav-link" href="https://alhooimeijer.wixsite.com/beenthere" target="_blank">About</a>
                         </li>
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Account</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Settings</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Log out</a>
-                        </li>
+                        {/*<li className="nav-item active">*/}
+                        {/*    <a className="nav-link" href="#">Account</a>*/}
+                        {/*</li>*/}
+                        {/*<li className="nav-item">*/}
+                        {/*    <a className="nav-link" href="#">Settings</a>*/}
+                        {/*</li>*/}
+                        {/*<li className="nav-item">*/}
+                        {/*    <a className="nav-link" href="#">Log out</a>*/}
+                        {/*</li>*/}
                     </ul>
                     {/*<span className="navbar-text">*/}
                     {/*  Navbar text with an inline elements*/}
