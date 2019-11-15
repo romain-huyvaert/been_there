9fef1f243e36e377cca0e1cbf713ddf3995e7a2b
Alex Post <alexpost1995@gmail.com>
2019-11-15 14:13:32 +0100

    Updated serviceWorker, close button on multiple reviews

--- been_there_frontend/basic/src/Mapbox.js
+++ been_there_frontend/basic/src/Mapbox.js
@@ -103,19 +103,15 @@ export default class Mapbox extends React.Component {
 
             if (name != undefined){
                 component.setState({popupOpened: true});
-                popup.innerHTML = "<img src=\"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fadmissions.colostate.edu%2Fmedia%2Fsites%2F19%2F2014%2F07%2Ficon_silhouette-01-1024x1024.png&f=1&nofb=1\" alt=" + reviewerName + "  align='left'> <div> <p class='reviewernaam'> " + reviewerName + " </p> </div>      <div class='reviewtekst' align='left'> <h2 class='bold'>" + name + "</h2>" + review + "<br />" + '<span class="' + "stars-container stars-" + rating * 20 + '">★★★★★</span> <button id="popupCloseButton" type="button" class="close" aria-label="Close">\n' +
-                    '  <span aria-hidden="true">&times;</span>\n' +
-                    '</button> </div>';
-
-                document.getElementById("popupCloseButton").addEventListener("click", function (e) {
-                    popup.style.display = "none";
-                });
+                popup.innerHTML = "<img src=\"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fadmissions.colostate.edu%2Fmedia%2Fsites%2F19%2F2014%2F07%2Ficon_silhouette-01-1024x1024.png&f=1&nofb=1\" alt=" + reviewerName + "  align='left'> <div> <p class='reviewernaam'> " + reviewerName + " </p> </div>      <div class='reviewtekst' align='left'> <h2 class='bold'>" + name + "</h2>" + review + "<br />" + '<span class="' + "stars-container stars-" + rating * 20 + '">★★★★★</span> ' +
+                    '<button id="popupCloseButton" type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button> </div>';
             }
             if (e.features[0].properties.first != undefined && e.features[0].properties.second != undefined && e.features[0].properties.third == undefined){
                 component.setState({popupOpened: true});
                 let firstObject = JSON.parse(e.features[0].properties.first);
                 let secondObject = JSON.parse(e.features[0].properties.second);
                 popup.innerHTML = "<img src=\"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fadmissions.colostate.edu%2Fmedia%2Fsites%2F19%2F2014%2F07%2Ficon_silhouette-01-1024x1024.png&f=1&nofb=1\" alt=" + firstObject.reviewerName + "  align='left'> <div> <p class='reviewernaam'> " + firstObject.reviewerName + " </p> </div>      <div class='reviewtekst' align='left'> <h2 class='bold'>" + firstObject.name + "</h2>" + firstObject.review + "<br />" + '<span class="' + "stars-container stars-" + firstObject.rating * 20 + '">★★★★★</span> </div>' +
+                    '<button id="popupCloseButton" type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button> </div>' +
                     "<hr /><br />" +
                     "<img src=\"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fadmissions.colostate.edu%2Fmedia%2Fsites%2F19%2F2014%2F07%2Ficon_silhouette-01-1024x1024.png&f=1&nofb=1\" alt=" + secondObject.reviewerName + "  align='left'> <div> <p class='reviewernaam'> " + secondObject.reviewerName + " </p> </div>      <div class='reviewtekst' align='left'> <h2 class='bold'>" + secondObject.name + "</h2>" + secondObject.review + "<br />" + '<span class="' + "stars-container stars-" + secondObject.rating * 20 + '">★★★★★</span> </div>'
             }
@@ -125,12 +121,16 @@ export default class Mapbox extends React.Component {
                 let secondObject = JSON.parse(e.features[0].properties.second);
                 let thirdObject = JSON.parse(e.features[0].properties.third);
                 popup.innerHTML = "<img src=\"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fadmissions.colostate.edu%2Fmedia%2Fsites%2F19%2F2014%2F07%2Ficon_silhouette-01-1024x1024.png&f=1&nofb=1\" alt=" + firstObject.reviewerName + "  align='left'> <div> <p class='reviewernaam'> " + firstObject.reviewerName + " </p> </div>      <div class='reviewtekst' align='left'> <h2 class='bold'>" + firstObject.name + "</h2>" + firstObject.review + "<br />" + '<span class="' + "stars-container stars-" + firstObject.rating * 20 + '">★★★★★</span> </div>' +
+                    '<button id="popupCloseButton" type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button> </div>' +
                     "<hr /><br />" +
                     "<img src=\"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fadmissions.colostate.edu%2Fmedia%2Fsites%2F19%2F2014%2F07%2Ficon_silhouette-01-1024x1024.png&f=1&nofb=1\" alt=" + secondObject.reviewerName + "  align='left'> <div> <p class='reviewernaam'> " + secondObject.reviewerName + " </p> </div>      <div class='reviewtekst' align='left'> <h2 class='bold'>" + secondObject.name + "</h2>" + secondObject.review + "<br />" + '<span class="' + "stars-container stars-" + secondObject.rating * 20 + '">★★★★★</span> </div>' +
                     "<hr /><br />" +
                     "<img src=\"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fadmissions.colostate.edu%2Fmedia%2Fsites%2F19%2F2014%2F07%2Ficon_silhouette-01-1024x1024.png&f=1&nofb=1\" alt=" + thirdObject.reviewerName + "  align='left'> <div> <p class='reviewernaam'> " + thirdObject.reviewerName + " </p> </div>      <div class='reviewtekst' align='left'> <h2 class='bold'>" + thirdObject.name + "</h2>" + thirdObject.review + "<br />" + '<span class="' + "stars-container stars-" + thirdObject.rating * 20 + '">★★★★★</span> </div>'
             }
 
+            document.getElementById("popupCloseButton").addEventListener("click", function (e) {
+                popup.style.display = "none";
+            });
             e.preventDefault();
 
             popup.style.display = "block";
@@ -201,38 +201,6 @@ export default class Mapbox extends React.Component {
                     var coordinates = [];
                     coordinates.push(lng, lat);
                     var layerId = "newPoint"+component.state.index++;
-
-                    // map.addLayer({
-                    //     "id": layerId,
-                    //     "type": "circle",
-                    //     "source": {
-                    //         "type": "geojson",
-                    //         "data": {
-                    //             "type": "FeatureCollection",
-                    //             "features": [{
-                    //                 "type": "Feature",
-                    //                 "properties": {
-                    //                     "name": name,
-                    //                     "rating": rating,
-                    //                     "review": review,
-                    //                     "reviewer": '123456',
-                    //                     "reviewerName": 'random dude',
-                    //                     "icon": "rocket"
-                    //                 },
-                    //                 "geometry": {
-                    //                     "type": "Point",
-                    //                     "coordinates": coordinates
-                    //                 }
-                    //             }]
-                    //         }
-                    //     },
-                    //     paint: {
-                    //         'circle-color': '#cc000b',
-                    //         'circle-radius': 8,
-                    //         'circle-stroke-width': 1,
-                    //         'circle-stroke-color': '#ffcf4b'
-                    //     },
-                    // });
 
                     map.addLayer({
                         "id": layerId,
