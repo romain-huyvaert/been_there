4459bd6c9066894a65efc96c5ce4c02d0b2a65f8
Romain Huyvaert <romain.huyvaert@gmail.com>
2019-11-26 15:22:01 +0100

    Deleting useless files

--- been_there_backend/credentials.py
+++
@@ -1,8 +0,0 @@
-# If this is the first time you are using this application please fill all the information
-# needed in this file to connect to the database
-
-name = 'postgres'
-user = 'BTMaster'
-password = 'BCtn2jAFZL'
-host = 'been-there.ciihojrwzxfz.eu-west-3.rds.amazonaws.com'
-port = '5445'
